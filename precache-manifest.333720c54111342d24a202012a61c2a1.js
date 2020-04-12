6357302205255bcd75547ee37b7d4d1055ade34a
sdf <dfvsdfvadv>
2020-04-12 19:57:04 +0200

    Updates

---
+++ precache-manifest.333720c54111342d24a202012a61c2a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9655df6b9bf2b51d229f102b43095f3f",
+    "url": "/My-Person-Manager/index.html"
+  },
+  {
+    "revision": "697f0aab8805c5981371",
+    "url": "/My-Person-Manager/static/css/2.61b51ee5.chunk.css"
+  },
+  {
+    "revision": "4ddd5e8cd4a88a7be996",
+    "url": "/My-Person-Manager/static/css/main.24d44e7d.chunk.css"
+  },
+  {
+    "revision": "697f0aab8805c5981371",
+    "url": "/My-Person-Manager/static/js/2.063b76ca.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/My-Person-Manager/static/js/2.063b76ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ddd5e8cd4a88a7be996",
+    "url": "/My-Person-Manager/static/js/main.b21c00f8.chunk.js"
+  },
+  {
+    "revision": "d48b5bd03e969dfa741a",
+    "url": "/My-Person-Manager/static/js/runtime-main.8ebfce35.js"
+  }
+]);
